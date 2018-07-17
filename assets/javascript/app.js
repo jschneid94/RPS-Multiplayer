a23db9f1862b0f379cfb38ce389f9081b0ad46a3
SchneiderMan <jschneid94@gmail.com>
2018-07-17 15:15:16 -0700

    Testing the google sign in.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,10 +13,17 @@ var database = firebase.database();
 $("#signIn").on("click", function() {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect(provider).then(function(result) {
+        // if (result.credential) {
+        //     // This gives you a Google Access Token. You can use it to access the Google API.
+        //     var token = result.credential.accessToken;
+        //     // ...
+        //   }
+        // The signed-in user info.
+        //var user = result.user;
         console.log(result);
         console.log("Log in successful");
-        $(".container").show();
-        $("")
+        $("#pageContent").show();
+        $("#signIn").hide();
     }).catch(function(err) {
         console.log(err);
         console.log("Log in failed");
